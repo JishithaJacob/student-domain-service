1cb923a596f55becc5772b6ff791778d853fe5e7
JishithaJacob <jishithavjacob@gmail.com>
2023-04-05 14:21:30 +0100

    first commit

---
+++ src/main/java/io/project/studentdomainservice/StudentDomainServiceApplication.java
@@ -0,0 +1,13 @@
+package io.project.studentdomainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDomainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDomainServiceApplication.class, args);
+	}
+
+}
