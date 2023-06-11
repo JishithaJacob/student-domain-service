0ae9892febb4e2be1338880c438b99b4931ebf13
JishithaJacob <jishithavjacob@gmail.com>
2023-06-11 09:12:05 +0100

    Spring Security- JdbcUserDetailsManagaer- H2 database

--- src/main/java/io/project/studentdomainservice/config/SecurityConfig.java
+++ src/main/java/io/project/studentdomainservice/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package io.project.studentdomainservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,11 +12,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
-import java.util.Collection;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,8 +29,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
+    EmbeddedDatabase dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("labsignoffDB")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
 
-   @Bean
+    @Bean
+    JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("my_super_secret_password_1234_$%@!"))
+                .roles("ADMIN")
+                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+    }
+
+
+  /* @Bean
     InMemoryUserDetailsManager users(){
        UserDetails user1= User.withUsername("user1")
                .password("{noop}password1")
@@ -37,18 +65,23 @@ public class SecurityConfig {
        usersList.add(user1);
        usersList.add(user2);
        return new InMemoryUserDetailsManager(usersList);
-    }
+    }*/
 
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-            return http.authorizeRequests(auth -> auth.anyRequest().authenticated())
-
-
+            return http.csrf(csrf-> csrf.ignoringRequestMatchers("/h2-console/**"))
+                    .authorizeRequests(auth -> auth
+                            .requestMatchers("/h2-console/**").permitAll()
+                            .anyRequest().authenticated())
+                    .headers(headers -> headers.frameOptions().sameOrigin())
                     .formLogin(withDefaults())
                     .build();
-
-
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
